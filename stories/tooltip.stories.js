ff6bb717041727c9f0f8dc338e3be74f073ed42f
axeleriksson147 <axel.eriksson@symphony.com>
2020-11-24 19:23:26 +0100

    Moved current .tk-tooltip styling to .tk-hint and updated .tk-tooltip

--- stories/tooltip.stories.js
+++ stories/tooltip.stories.js
@@ -2,18 +2,31 @@ export default {
     title: 'Tooltip',
 };
 
+export const Hint = () => {
+    return (`
+    <div style="min-height: 600px">
+      <h2>Hint</h2>
+        <div id="hint-vZUXsOqChv" role="hint" class="tk-hint" style="max-width: 400px; position: relative">
+            <span class="tk-hint__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+            <div style="position: absolute; left: calc(50% - 10px); bottom: -32px;">
+                <div class="tk-hint__arrow" style="transform: rotate(45deg);"></div>       
+            </div>
+            <div class="tk-hint__footer">
+                <span class="tk-hint__close" style="cursor: pointer">Got it</span>
+            </div>
+        </div>
+      </div>
+    `);
+};
+
 export const Tooltip = () => {
     return (`
     <div style="min-height: 600px">
       <h2>Tooltip</h2>
         <div id="hint-vZUXsOqChv" role="tooltip" class="tk-tooltip" style="max-width: 400px; position: relative">
-            <span class="tk-tooltip__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-            <div style="position: absolute; left: calc(50% - 10px); bottom: -32px;">
-                <div class="tk-tooltip__arrow" style="transform: rotate(45deg);"></div>       
-            </div>
-            <div class="tk-tooltip__footer">
-                <span class="tk-tooltip__close" style="cursor: pointer">Got it</span>
-            </div>
+            <span class="tk-tooltip__description">
+                Tooltip
+            </span>
         </div>
       </div>
     `);
