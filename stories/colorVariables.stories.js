f033829548902dffa70d95a322da14792385b80b
AliciaSymphony <alicia.marin@symphony.com>
2021-06-22 11:51:10 +0200

    APP-4244 feat(colors): rename add -color- to the prefix

--- stories/colorVariables.stories.js
+++ stories/colorVariables.stories.js
@@ -28,15 +28,15 @@ export const ColorVariables = () => {
   return `<div> <h2>Main colors</h2>
   <p>Click to copy the variable to your clipboard</p>
     ${mainColors.map((color) => {
-      const hex = getHex(`--tk-${color}`);
+      const hex = getHex(`--tk-color-${color}`);
 
       return `<div
         class="tk-card"
-        style="background-color: var(--tk-${color}); cursor: pointer; max-width: 30rem;"
-        onclick="navigator.clipboard.writeText('--tk-${color}')"
+        style="background-color: var(--tk-color-${color}); cursor: pointer; max-width: 30rem;"
+        onclick="navigator.clipboard.writeText('--tk-color-${color}')"
       >
         <div class="tk-m-1" style="display: flex; justify-content: space-between">
-          <span style="color: #000"> --tk-${color} ${hex}</span> <span style="color: #fff">--tk-${color} ${hex}</<span>
+          <span style="color: #000"> --tk-color-${color} ${hex}</span> <span style="color: #fff">--tk-color-${color} ${hex}</<span>
         </div>
       </div>`;
     }).join('<br/>')}
@@ -45,14 +45,14 @@ export const ColorVariables = () => {
           `<div><h2>${color.toUpperCase()}</h2>
           <p>Click to copy the variable to your clipboard</p>
           ${scales.map((scale) => {
-            const hex = getHex(`--tk-${color}-${scale}`);
+            const hex = getHex(`--tk-color-${color}-${scale}`);
             return `<div
               class="tk-card"
-              style="background-color: var(--tk-${color}-${scale}); cursor: pointer; max-width: 30rem;"
-              onclick="navigator.clipboard.writeText('--tk-${color}-${scale}')"
+              style="background-color: var(--tk-color-${color}-${scale}); cursor: pointer; max-width: 30rem;"
+              onclick="navigator.clipboard.writeText('--tk-color-${color}-${scale}')"
               >
               <div class="tk-m-1" style="display: flex; justify-content: space-between">
-              <span style="color: #000"> --tk-${color}-${scale} ${hex}</span> <span style="color: #fff">--tk-${color}-${scale} ${hex}</<span>
+              <span style="color: #000"> --tk-color-${color}-${scale} ${hex}</span> <span style="color: #fff">--tk-color-${color}-${scale} ${hex}</<span>
               </div>
             </div>`;
           }).join('<br/>')}
